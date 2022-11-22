5cef92845a9d8b38ce87fb49419528751f3cb107
Agustin Gibertoni <adgibertoni@gmail.com>
2022-11-22 17:42:20 -0300

    added test in picture + img in top10 + entradas a la db

--- nasa_blog/top10/models.py
+++ nasa_blog/top10/models.py
@@ -9,16 +9,18 @@ class Top10(models.Model):
     title = models.CharField(max_length=70, null=False, blank=False)
     header = RichTextField(null=False, blank=False)
     content = RichTextField(null=True, blank=True)
+    year = models.IntegerField(null=False, blank=False, default=2000)
+    image = models.ImageField(upload_to='top10', null=True, blank=True)
     created_at = models.DateTimeField (auto_now_add=True)
     updated_at = models.DateTimeField (auto_now=True)
 
     class Meta:
         unique_together = (
             "title",
-            "header",
+            "year",
         )
         ordering = ["-created_at"]
 
     def __str__(self):
-        return f"Noticia: {self.title}"
+        return f"Noticia: {self.title}, {self.year}"
 
